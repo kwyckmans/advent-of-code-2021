291c5876f6fc7c9e6ac622e544cd0658f6cf9b3b
Kevin Wyckmans <kwyckmans@spotify.com>
2021-12-02 15:49:14 +0100

    experiment with some clean-up

--- day2/day2.py
+++ day2/day2.py
@@ -1,4 +1,5 @@
-from typing import List
+from typing import List, NamedTuple
+from dataclasses import dataclass
 
 test_input = """forward 5
 down 5
@@ -27,20 +28,26 @@ def calculate_position(instructions) -> List[int]:
 position = calculate_position(instructions)
 print(f"The answer for day 2 - 1 is {position[0] * position[1]}")
 
+@dataclass
+class Position:
+    x: int = 0
+    y: int = 0
+
 def calculate_position_with_aim(instructions) -> List[int]:
-    position = [0, 0]
+    position = Position()
     aim = 0
 
     for instruction in instructions:
-        match instruction[0]:
+        command, value = instruction
+        match command:
             case "forward":
-                position[0] = position[0] + instruction[1]
-                position[1] = position[1] + instruction[1] * aim if aim > 0 else position[1]
-            case "down": aim = aim + instruction[1]
-            case "up": aim = aim - instruction[1]
+                position.x = position.x + value
+                position.y = position.y + value * aim if aim > 0 else position.y
+            case "down": aim = aim + value
+            case "up": aim = aim - value
             case _: raise ValueError("Unkown command")
 
     return position
 
 position = calculate_position_with_aim(instructions)
-print(f"Your position is {position}, this makes the answer {position[0] * position[1]}")
+print(f"Your position is {position}, this makes the answer {position.x * position.y}")
